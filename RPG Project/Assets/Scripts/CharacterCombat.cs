df2d81813c5fcaada40b32f014ba13cd0e55d9c9
Isaiah-Mck <Mr.isaiahmck@gmail.com>
2022-02-05 23:23:23 -0800

    Attack Animations Added

--- RPG Project/Assets/Scripts/CharacterCombat.cs
+++ RPG Project/Assets/Scripts/CharacterCombat.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 public class CharacterCombat : MonoBehaviour{
 
     public float attackSpeed = 1f;
-    private float attackCooldown = 0f; 
+    private float attackCooldown = 0f;
+    const float combatCooldown = 5;
+    float lastAttackTime;  
 
     public float attackDelay = .6f;
 
+    public bool InCombat {get; private set; }
     public event System.Action OnAttack;
 
     CharacterStats myStats;
@@ -20,6 +23,10 @@ public class CharacterCombat : MonoBehaviour{
 
     void Update(){
         attackCooldown -= Time.deltaTime;
+
+        if (Time.time - lastAttackTime > combatCooldown){
+            InCombat = false;
+        }
     }
 
     public void Attack (CharacterStats targetStats){
@@ -31,6 +38,8 @@ public class CharacterCombat : MonoBehaviour{
                 OnAttack();
             
             attackCooldown = 1f / attackSpeed;
+            InCombat = true;
+            lastAttackTime = Time.time; 
         }
     }
 
@@ -38,5 +47,9 @@ public class CharacterCombat : MonoBehaviour{
         yield return new WaitForSeconds(delay);
 
         stats.TakeDamage(myStats.damage.GetValue());
+
+        if (stats.currentHealth <= 0){
+            InCombat = false; 
+        }
     }
 }
